0e5e0318fccce7a9fb4c4a6865feaf68bb34d69f
Arch <yuhaow29@uci.edu>
2025-11-18 17:11:26 -0800

    add globalVar.cpp

---
+++ src/event/globalVar.cpp
@@ -0,0 +1,10 @@
+#include "globalVar.h"
+
+std::string title = "";
+std::string artist = "";
+std::string album = "";
+float total_time = 0;
+float current_time = 0;
+size_t term_height = 0;
+size_t term_width = 0;
+size_t theme_color = 0;
