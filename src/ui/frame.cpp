2911738005163e4e030a7196fb99c4bb4d39f242
Arch <yuhaow29@uci.edu>
2025-11-18 16:33:41 -0800

    add frame.cpp and frame.h

---
+++ src/ui/frame.cpp
@@ -0,0 +1,9 @@
+
+
+const char* downRight = "┏";
+const char* upRight = "┗";
+const char* downLeft = "┓";
+const char* upLeft = "┛";
+const char* horiLine = "━";
+const char* vertiLine = "┃";
+const char* 
