666ac2b7d2c3f5beae3f8f250c175f69e7e59179
Arch <yuhaow29@uci.edu>
2025-11-18 16:49:51 -0800

    add globalVar.h

---
+++ src/event/globalVar.h
@@ -0,0 +1,15 @@
+#ifndef GLOBALVAR_H
+#define GLOBALVAR_H
+
+#include <string>
+
+inline extern std::string title;
+inline extern std::string artist;
+inline extern std::string album;
+inline extern std::string total_time;
+inline extern std::string current_time;
+inline extern size_t term_height;
+inline extern size_t term_width;
+inline extern size_t theme_color;
+
+#endif //GLOBALVAR_H
