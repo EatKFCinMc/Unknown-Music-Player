2fa973c09d8dc07b306785dcfec9af6d08b2bc7d
Arch <yuhaow29@uci.edu>
2026-05-07 22:29:29 -0700

    update logger with timestamp

--- src/logger/log.cpp
+++ src/logger/log.cpp
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <filesystem>
+#include <thread>
+#include <ctime>
 
 #include "../event/globalVar.h"
 
@@ -10,11 +12,28 @@ void log_init() {
         std::filesystem::remove("ump.log");
 }
 
-void logger(const std::string &message) {
+// Get the current date/time. The format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime() {
+    time_t now = time(0);
+    struct tm tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    return buf;
+}
+
+void log_thread(const std::string &message) {
     // Simple logging to a file
     if (LOGGER_DISABLED) return;
+    std::string buf = currentDateTime();
+    buf = "[" + buf + "]: " + message;
     FILE *logFile = fopen("ump.log", "a");
-    if (logFile != NULL)
-        fprintf(logFile, "%s\n", message.c_str());
+    if (logFile != nullptr)
+        fprintf(logFile, "%s\n", buf.c_str());
     fclose(logFile);
+}
+
+void logger(const std::string &message) {
+    std::thread log_t(log_thread, message);
+    log_t.detach();
 }
